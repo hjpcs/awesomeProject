636d3b7895561bdd4ef95785cde0ae9a860ed46b
Jinpeng He <jinpeng.he@shopee.com>
2019-12-26 18:27:36 +0800

    init

---
+++ src/ch14/error/err_test.go
@@ -0,0 +1,38 @@
+package error
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+var LessThanTwoError = errors.New("n should be not less than 2")
+var LargerThanHundredError = errors.New("n should be not larger than 100")
+
+func GetFibonacci(n int) ([]int, error) {
+	if n < 0 {
+		return nil, LessThanTwoError
+	}
+	if n > 100 {
+		return nil, LargerThanHundredError
+	}
+	fibList := []int{1, 1}
+	for i := 2; i < n; i++ {
+		fibList = append(fibList, fibList[i-2]+fibList[i-1])
+	}
+	return fibList, nil
+}
+
+func TestGetFibonacci(t *testing.T) {
+	if v, err := GetFibonacci(-1); err != nil {
+		if err == LessThanTwoError {
+			fmt.Println("it is less")
+		}
+		if err == LargerThanHundredError {
+			fmt.Println("it is larger")
+		}
+		t.Error(err)
+	} else {
+		t.Log(v)
+	}
+}
