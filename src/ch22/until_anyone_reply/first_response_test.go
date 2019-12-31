56f66816cc1d448e2d9a72af5e6a10c219ab262a
Jinpeng He <jinpeng.he@shopee.com>
2019-12-31 19:01:24 +0800

    init

---
+++ src/ch22/until_anyone_reply/first_response_test.go
@@ -0,0 +1,55 @@
+package until_anyone_reply
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+	"time"
+)
+
+func runTask(id int) string {
+	time.Sleep(10 * time.Millisecond)
+	return fmt.Sprintf("the result is from %d", id)
+}
+
+func FirstResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	return <-ch
+}
+
+func AllResponse() string {
+	numOfRunner := 10
+	ch := make(chan string, numOfRunner)
+	for i := 0; i < numOfRunner; i++ {
+		go func(i int) {
+			ret := runTask(i)
+			ch <- ret
+		}(i)
+	}
+	finalRet := ""
+	for j := 0; j < numOfRunner; j++ {
+		finalRet += <-ch + "\n"
+	}
+	return finalRet
+}
+
+func TestFirstResponse(t *testing.T) {
+	t.Log("before:", runtime.NumGoroutine())
+	t.Log(FirstResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("after:", runtime.NumGoroutine())
+}
+
+func TestAllResponse(t *testing.T) {
+	t.Log("before:", runtime.NumGoroutine())
+	t.Log(AllResponse())
+	time.Sleep(time.Second * 1)
+	t.Log("after:", runtime.NumGoroutine())
+}
